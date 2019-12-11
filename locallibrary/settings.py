a6d3f7c4cc04a8d51751abd088524cc0a07e43df
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2019-12-11 01:23:32 -0300

    retirei o django_tables2

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Projetos',
-    'django_tables2',
+    'Projetos',    
     'account',
 ]
 
